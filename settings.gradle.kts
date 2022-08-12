7ee058ef3792ed942274bfbcc2615d2e651c9d6e
Yusuf Ruziev <planetapluton888@gmail.com>
2022-08-12 14:03:42 +0300

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "english.simpler.joseph.simpler-english-backend"
---
+++ src/main/kotlin/english/simpler/joseph/Application.kt
@@ -0,0 +1,11 @@
+package english.simpler.joseph
+
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import english.simpler.joseph.plugins.*
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
+        configureRouting()
+    }.start(wait = true)
+}
