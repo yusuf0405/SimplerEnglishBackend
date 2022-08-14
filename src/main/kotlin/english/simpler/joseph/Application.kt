ddc72d468f5daf525ea47d856b5334e83d7183f5
Yusuf Ruziev <planetapluton888@gmail.com>
2022-08-14 12:14:05 +0300

    Delete class DatabaseConnection.kt

--- src/main/kotlin/english/simpler/joseph/Application.kt
+++ src/main/kotlin/english/simpler/joseph/Application.kt
@@ -2,13 +2,11 @@ package english.simpler.joseph
 
 import com.zaxxer.hikari.HikariConfig
 import com.zaxxer.hikari.HikariDataSource
-import english.simpler.joseph.database.DatabaseConnection
 import english.simpler.joseph.features.login.configureLoginRouting
 import english.simpler.joseph.features.register.configureRegisterRouting
 import english.simpler.joseph.plugins.configureRouting
 import english.simpler.joseph.plugins.configureSerialization
 import io.ktor.server.engine.*
-import io.ktor.server.cio.*
 import io.ktor.server.netty.*
 import org.jetbrains.exposed.sql.Database
 
@@ -16,7 +14,6 @@ fun main() {
     val config = HikariConfig("hikari.properties")
     val dataSource = HikariDataSource(config)
     Database.connect(dataSource)
-    DatabaseConnection.database
 
     embeddedServer(Netty, port = System.getenv("PORT").toInt()) {
         configureRouting()
